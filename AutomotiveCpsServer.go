a318f2ba28ac897413f5030a8d2bc08eb4406ae1
Gregory Maldonado <gmaldona@oswego.edu>
2022-11-14 13:27:11 -0500

    cleaner info and error logs

--- AutomotiveCpsServer.go
+++ AutomotiveCpsServer.go
@@ -1,16 +1,18 @@
-package main
-
-/* A tcp client that acts as a middle man between ANKI Drive vehicles and the ANKI Drive SDK for Java.
+/*
+ * State University of New York, College at Oswego
+ *
+ * A tcp client that acts as a middle man between ANKI Drive vehicles and the ANKI Drive SDK for Java.
  * Forms a tcp/ip connection to the SDK and uses tinygo BLE module for connecting to each ANKI Drive vehicle.
  * ANKI Drive vehicle firmware and message protocol can be found here:
  *		https://github.com/tenbergen/anki-drive-java/blob/master/Anki%20Drive%20Programming%20Guide.pdf
  *
  * Date:    November 13, 2022
- * Author:  Bastian Tenbergen & Gregory Maldonado
+ * Author:  Bastian Tenbergen, PhD & Gregory Maldonado
  * Version: 1.0
-
- * State University of New York College at Oswego
+ *
  */
+
+package main
 
 import (
 	"bytes"
@@ -21,16 +23,22 @@ import (
 	"io/ioutil"
 	"log"
 	"net"
-	"os"
 	"regexp"
 	"strings"
 	"time"
 	"tinygo.org/x/bluetooth"
 )
 
+const (
+	ANSI_RESET = "\u001B[0m"
+	ANSI_RED   = "\u001B[31m"
+	ANSI_GREEN = "\u001B[32m"
+)
+
 var (
 	server                  Server
 	Adapter                 = bluetooth.DefaultAdapter
+	AdapterEnabled          = false
 	ANKI_STR_SERVICE_UUID   = bluetooth.NewUUID([16]byte{0xBE, 0x15, 0xBE, 0xEF, 0x61, 0x86, 0x40, 0x7E, 0x83, 0x81, 0x0B, 0xD8, 0x9C, 0x4D, 0x8D, 0xF4})
 	ANKI_STR_CHR_READ_UUID  = bluetooth.NewUUID([16]byte{0xBE, 0x15, 0xBE, 0xE0, 0x61, 0x86, 0x40, 0x7E, 0x83, 0x81, 0x0B, 0xD8, 0x9C, 0x4D, 0x8D, 0xF4})
 	ANKI_STR_CHR_WRITE_UUID = bluetooth.NewUUID([16]byte{0xBE, 0x15, 0xBE, 0xE1, 0x61, 0x86, 0x40, 0x7E, 0x83, 0x81, 0x0B, 0xD8, 0x9C, 0x4D, 0x8D, 0xF4})
@@ -55,41 +63,39 @@ type ServerConf struct {
 }
 
 func main() {
+	server.DiscoveredDevices = cmap.New[AnkiVehicle]()
+	server.ConnectedDevices = cmap.New[*bluetooth.Device]()
+	server.DeviceCharacteristics = cmap.New[[]bluetooth.DeviceCharacteristic]()
 
 	file, err := ioutil.ReadFile("serverconf.yml")
 	if err != nil {
-		log.Fatalln(err)
+		displayError(err.Error())
 	}
 
 	serverConf := ServerConf{}
 	err = yaml.Unmarshal(file, &serverConf)
 	if err != nil {
-		log.Fatalf(err.Error())
+		displayError(err.Error())
 	}
-
-	server.DiscoveredDevices = cmap.New[AnkiVehicle]()
-	server.ConnectedDevices = cmap.New[*bluetooth.Device]()
-	server.DeviceCharacteristics = cmap.New[[]bluetooth.DeviceCharacteristic]()
 
 	// Listen for connections on host and port
 	l, err := net.Listen("tcp", serverConf.Host+":"+serverConf.Port)
 	if err != nil {
-		log.Fatalln(err)
+		displayError(err.Error())
 	}
 
 	// terminate server on port when disconnected
 	defer func(l net.Listener) {
-		err := l.Close()
-		if err != nil {
-		}
+		l.Close()
 	}(l)
-	fmt.Println("Starting Server...\nListening on " + serverConf.Host + ":" + serverConf.Port)
+	displayInfo("Starting Server... Listening on " + serverConf.Host + ":" + serverConf.Port)
 	for {
 		// Listen for an incoming connection.
 		conn, err := l.Accept()
+		displayInfo("Connection established.")
+
 		if err != nil {
-			fmt.Println("Error accepting: ", err.Error())
-			os.Exit(1)
+			displayError(err.Error())
 		}
 		// Handle connections in a new goroutine.
 		go handleRequest(conn)
@@ -101,12 +107,18 @@ func handleRequest(conn net.Conn) {
 
 	// Keep grabbing messages from tcp connection until server termination
 	for {
-		// Make a buffer to hold incoming data.
-		buf := make([]byte, 1024)
 		// Read the incoming connection into the buffer.
+		buf := make([]byte, 1024)
 		_, err := conn.Read(buf)
+		// if err, then probably a client disconnect
 		if err != nil {
-			log.Fatalln("Error reading:", err.Error())
+			displayInfo("Client disconnect? Disconnecting all devices...")
+			for _, device := range server.ConnectedDevices.Items() {
+				device.Disconnect()
+			}
+			server.ConnectedDevices = cmap.New[*bluetooth.Device]()
+			conn.Close()
+			return
 		}
 
 		// Create a goroutine for incoming msg and listen for the next msg
@@ -127,25 +139,23 @@ func handleRequest(conn net.Conn) {
 				msg = set[1]
 			}
 
-			fmt.Println("BUFF: ", string(buf))
-
-			// Perform different actions based on the tcp msg recieved from ANKI SDK
+			// Perform different actions based on the tcp msg received from ANKI SDK
 			switch {
 			// SCAN request from java
 			case strings.Contains(string(buf), "SCAN"):
-				fmt.Println("Scanning...")
+				displayInfo("Scanning...")
 				// call scan function to search for nearby vehicles
 				server.DiscoveredDevices = scan()
 				for _, device := range server.DiscoveredDevices.Items() {
 					// for each found device, send a tcp msg to java saying found
 					conn.Write([]byte("SCAN;" + device.Address + ";" + device.ManufacturerData + ";" + device.LocalName + "\n"))
 
-					fmt.Println("Found " + device.Address)
+					displayInfo("Found device: " + device.Address)
 					time.Sleep(500 * time.Millisecond)
 				}
 				// Stops scanning on java side
 				conn.Write([]byte("SCAN;COMPLETED\n"))
-				fmt.Println("Scanning Completed.")
+				fmt.Println(ANSI_GREEN + "Scanning Completed." + ANSI_RESET)
 				return
 
 			//DISCONNECT request from java
@@ -155,11 +165,13 @@ func handleRequest(conn net.Conn) {
 				address := string(bytes.Trim([]byte(set[1]), "\x00"))
 				connectedDevice, ok := server.ConnectedDevices.Get(address)
 				if !ok {
-					log.Fatalln("Address: " + address)
+					displayError("Address: " + address + " could not be found.")
 				}
 				connectedDevice.Disconnect()
+				server.ConnectedDevices.Remove(address)
+
 				conn.Write([]byte("DISCONNECT;SUCCESS\n"))
-				fmt.Println(address + " Disconnected.")
+				displayInfo(address + " Disconnected.")
 
 			// CONNECT request from java
 			case strings.Contains(set[0], "CONNECT"):
@@ -171,17 +183,16 @@ func handleRequest(conn net.Conn) {
 				// connect to device
 				connectedDevice, err := Adapter.Connect(device.Addresser, bluetooth.ConnectionParams{})
 				if err != nil {
-					log.Fatalln(err.Error())
+					displayError(err.Error())
 				}
 
 				// add device to concurrent map of devices
 				server.ConnectedDevices.Set(device.Address, connectedDevice)
-				fmt.Println("Connected to", device.Address)
+				fmt.Println(ANSI_GREEN + "Connected to " + device.Address + ANSI_RESET)
 
 				services, _ := connectedDevice.DiscoverServices([]bluetooth.UUID{ANKI_STR_SERVICE_UUID})
 				if err != nil {
-					fmt.Println("Failed to discover services")
-					return
+					displayInfo(err.Error())
 				}
 
 				// Getting the writers and readers services
@@ -196,13 +207,12 @@ func handleRequest(conn net.Conn) {
 					encodedBytes := hex.EncodeToString(value)
 					// Send the vehicle respond back to java
 					conn.Write([]byte(device.Address + ";" + encodedBytes + "\n"))
-					fmt.Println("RECEIVED: [" + device.Address + ";" + encodedBytes + "]")
+					displayInfo("RECEIVED: [" + device.Address + ";" + encodedBytes + "]")
 				})
 
 				// terminate connection request to java
 				conn.Write([]byte("CONNECT;SUCCESS\n"))
-				fmt.Println("CONNECT COMPLETED")
-				return
+				fmt.Println(ANSI_GREEN + "CONNECT COMPLETED." + ANSI_RESET)
 
 			/* Any other request is assumed to be a command given to the car. Each byte in the buffer represents an action that is
 			outlined in https://github.com/tenbergen/anki-drive-java/blob/master/Anki%20Drive%20Programming%20Guide.pdf
@@ -217,11 +227,10 @@ func handleRequest(conn net.Conn) {
 					// write payload to anki vehicle
 					_, err := writeService.WriteWithoutResponse(payload)
 					if err != nil {
-						fmt.Println(err)
-						return
+						displayError(err.Error())
 					}
 
-					fmt.Println("SENDING: [" + strings.Replace(string(buf), "\n", "", -1) + "]")
+					displayInfo("SENDING: [" + strings.Replace(string(buf), "\n", "", -1) + "]")
 				}
 			}
 		}(buf)
@@ -235,7 +244,11 @@ func scan() cmap.ConcurrentMap[string, AnkiVehicle] {
 	channel := make(chan string, 1)
 	// func that is wrapped, so it can time out in some number of seconds
 	go func() {
-		must("enable BLE stack", Adapter.Enable())
+
+		if !AdapterEnabled {
+			must("enable BLE stack", Adapter.Enable())
+			AdapterEnabled = true
+		}
 
 		err := Adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
 			// only scan for devices that contain "Drive" for anki drive
@@ -256,15 +269,18 @@ func scan() cmap.ConcurrentMap[string, AnkiVehicle] {
 				}
 			}
 		})
-		must("start scan", err)
-		must("enable BLE stack", Adapter.StopScan())
+		if err != nil {
+			return
+		}
+		//must("start scan", err)
+		//must("enable BLE stack", Adapter.StopScan())
 
-		channel <- "finished scanning"
 	}()
 
 	// timeout scan
 	select {
 	case <-channel:
+		channel <- "break"
 		break
 	case <-time.After(5 * time.Second):
 		break
@@ -277,4 +293,13 @@ func must(action string, err error) {
 	if err != nil {
 		panic("failed to " + action + ": " + err.Error())
 	}
+}
+
+func displayInfo(msg string) {
+	fmt.Println(ANSI_GREEN + "[INFO] " + ANSI_RESET + msg)
+}
+
+func displayError(msg string) {
+	fmt.Print(ANSI_RED + "[ERROR] " + ANSI_RESET)
+	log.Fatalln(msg)
 }
